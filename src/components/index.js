1b8730f94a7396d68f6dd33b0acdf4cf44bfb5fa
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-04-14 12:58:53 +0200

    iphone model- react-three-r3f

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Hero } from "./Hero";
 export { default as Highlights } from "./Highlights";
 export { default as Model } from "./Model";
+export { default as ModelView } from "./ModelView";
 export { default as Navbar } from "./Navbar";
